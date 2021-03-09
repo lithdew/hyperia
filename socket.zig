21389c825469eaf74bd9c711f7d1be8204a76405
lithdew <kenta@lithdew.net>
2021-03-10 05:25:29 +0900

    socket/linux: add read timeout, write timeout, and accept with tests

--- socket.zig
+++ socket.zig
@@ -3,6 +3,8 @@ const std = @import("std");
 const os = std.os;
 const mem = std.mem;
 const net = std.net;
+const time = std.time;
+const testing = std.testing;
 
 const print = std.debug.print;
 
@@ -15,6 +17,10 @@ pub const Socket = struct {
 
     pub fn deinit(self: Socket) void {
         os.close(self.fd);
+    }
+
+    pub fn shutdown(self: Socket, how: os.ShutdownHow) !void {
+        try os.shutdown(self.fd, how);
     }
 
     pub fn bind(self: Socket, address: net.Address) !void {
@@ -39,7 +45,142 @@ pub const Socket = struct {
     pub fn getError(self: Socket) !void {
         try os.getsockoptError(self.fd);
     }
+
+    pub fn setReadBufferSize(self: Socket, size: u32) !void {
+        const rc = os.system.setsockopt(self.fd, os.SOL_SOCKET, os.SO_RCVBUF, mem.asBytes(&size), @sizeOf(u32));
+        return switch (os.errno(rc)) {
+            0 => {},
+            os.EBADF => error.BadFileDescriptor,
+            os.EFAULT => error.InvalidAddressSpace,
+            os.EINVAL => error.InvalidSocketOption,
+            os.ENOPROTOOPT => error.UnknownSocketOption,
+            os.ENOTSOCK => error.NotASocket,
+            else => |err| os.unexpectedErrno(err),
+        };
+    }
+
+    pub fn setWriteBufferSize(self: Socket, size: u32) !void {
+        const rc = os.system.setsockopt(self.fd, os.SOL_SOCKET, os.SO_SNDBUF, mem.asBytes(&size), @sizeOf(u32));
+        return switch (os.errno(rc)) {
+            0 => {},
+            os.EBADF => error.BadFileDescriptor,
+            os.EFAULT => error.InvalidAddressSpace,
+            os.EINVAL => error.InvalidSocketOption,
+            os.ENOPROTOOPT => error.UnknownSocketOption,
+            os.ENOTSOCK => error.NotASocket,
+            else => |err| os.unexpectedErrno(err),
+        };
+    }
+
+    pub fn setReadTimeout(self: Socket, milliseconds: usize) !void {
+        const timeout = os.timeval{
+            .tv_sec = @intCast(isize, milliseconds / time.ms_per_s),
+            .tv_usec = @intCast(isize, (milliseconds % time.ms_per_s) * time.us_per_ms),
+        };
+
+        const rc = os.system.setsockopt(self.fd, os.SOL_SOCKET, os.SO_RCVTIMEO, mem.asBytes(&timeout), @sizeOf(os.timeval));
+        return switch (os.errno(rc)) {
+            0 => {},
+            os.EBADF => error.BadFileDescriptor,
+            os.EFAULT => error.InvalidAddressSpace,
+            os.EINVAL => error.InvalidSocketOption,
+            os.ENOPROTOOPT => error.UnknownSocketOption,
+            os.ENOTSOCK => error.NotASocket,
+            else => |err| os.unexpectedErrno(err),
+        };
+    }
+
+    pub fn setWriteTimeout(self: Socket, milliseconds: usize) !void {
+        const timeout = os.timeval{
+            .tv_sec = @intCast(isize, milliseconds / time.ms_per_s),
+            .tv_usec = @intCast(isize, (milliseconds % time.ms_per_s) * time.us_per_ms),
+        };
+
+        const rc = os.system.setsockopt(self.fd, os.SOL_SOCKET, os.SO_SNDTIMEO, mem.asBytes(&timeout), @sizeOf(os.timeval));
+        return switch (os.errno(rc)) {
+            0 => {},
+            os.EBADF => error.BadFileDescriptor,
+            os.EFAULT => error.InvalidAddressSpace,
+            os.EINVAL => error.InvalidSocketOption,
+            os.ENOPROTOOPT => error.UnknownSocketOption,
+            os.ENOTSOCK => error.NotASocket,
+            else => |err| os.unexpectedErrno(err),
+        };
+    }
+
+    pub const Connection = struct {
+        socket: Socket,
+        address: net.Address,
+    };
+
+    pub fn accept(self: Socket, flags: u32) !Connection {
+        var address: os.sockaddr = undefined;
+        var address_len: u32 = @sizeOf(os.sockaddr);
+
+        const fd = try os.accept(self.fd, &address, &address_len, flags);
+
+        return Connection{
+            .socket = Socket{ .fd = fd },
+            .address = net.Address.initPosix(@alignCast(4, &address)),
+        };
+    }
 };
+
+test "socket/linux: set write timeout" {
+    const a = try Socket.init(os.AF_INET, os.SOCK_STREAM | os.SOCK_CLOEXEC, os.IPPROTO_TCP);
+    defer a.deinit();
+
+    try a.bind(net.Address.initIp4([_]u8{ 0, 0, 0, 0 }, 0));
+    try a.listen(128);
+
+    const binded_address = try a.getName();
+    print("Binded to address: {}\n", .{binded_address});
+
+    const b = try Socket.init(os.AF_INET, os.SOCK_STREAM | os.SOCK_CLOEXEC, os.IPPROTO_TCP);
+    defer b.deinit();
+
+    try b.connect(binded_address);
+
+    const ab = try a.accept(os.SOCK_CLOEXEC);
+    defer ab.socket.deinit();
+
+    // The minimum read buffer size is 128.
+    // The minimum write buffer size is 1024.
+    // All buffer sizes are doubled when they are passed in.
+    // After taking into account book-keeping for buffer sizes, the minimum
+    // buffer size before writes start to block the main thread is 65,483 bytes.
+
+    var buf: [65_483]u8 = undefined;
+
+    try ab.socket.setReadBufferSize(128);
+    try b.setWriteBufferSize(1024);
+    try b.setWriteTimeout(10);
+
+    testing.expectEqual(buf.len - 1, try os.write(b.fd, &buf));
+}
+
+test "socket/linux: set read timeout" {
+    const a = try Socket.init(os.AF_INET, os.SOCK_STREAM | os.SOCK_CLOEXEC, os.IPPROTO_TCP);
+    defer a.deinit();
+
+    try a.bind(net.Address.initIp4([_]u8{ 0, 0, 0, 0 }, 0));
+    try a.listen(128);
+
+    const binded_address = try a.getName();
+    print("Binded to address: {}\n", .{binded_address});
+
+    const b = try Socket.init(os.AF_INET, os.SOCK_STREAM | os.SOCK_CLOEXEC, os.IPPROTO_TCP);
+    defer b.deinit();
+
+    try b.connect(binded_address);
+    try b.setReadTimeout(10);
+
+    const ab = try a.accept(os.SOCK_CLOEXEC);
+    defer ab.socket.deinit();
+
+    var buf: [1]u8 = undefined;
+    testing.expectError(error.WouldBlock, os.read(b.fd, &buf));
+}
 
 test "socket/linux: create socket pair" {
     const a = try Socket.init(os.AF_INET, os.SOCK_STREAM | os.SOCK_NONBLOCK | os.SOCK_CLOEXEC, os.IPPROTO_TCP);
@@ -54,12 +195,11 @@ test "socket/linux: create socket pair" {
     const b = try Socket.init(os.AF_INET, os.SOCK_STREAM | os.SOCK_NONBLOCK | os.SOCK_CLOEXEC, os.IPPROTO_TCP);
     defer b.deinit();
 
-    b.connect(binded_address) catch |err| switch (err) {
-        error.WouldBlock => {},
-        else => return err,
-    };
-
+    testing.expectError(error.WouldBlock, b.connect(binded_address));
     try b.getError();
+
+    const ab = try a.accept(os.SOCK_NONBLOCK | os.SOCK_CLOEXEC);
+    defer ab.socket.deinit();
 }
 
 test "raw_socket/linux: create socket pair" {
@@ -84,10 +224,6 @@ test "raw_socket/linux: create socket pair" {
     const b = try os.socket(os.AF_INET, os.SOCK_STREAM | os.SOCK_NONBLOCK | os.SOCK_CLOEXEC, os.IPPROTO_TCP);
     defer os.close(b);
 
-    os.connect(b, &binded_address, binded_address_len) catch |err| switch (err) {
-        error.WouldBlock => {},
-        else => return err,
-    };
-
+    testing.expectError(error.WouldBlock, os.connect(b, &binded_address, binded_address_len));
     try os.getsockoptError(b);
 }
