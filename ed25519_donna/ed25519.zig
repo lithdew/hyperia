9de3f53f0d417e3e0361e1af2955e3ad18fd0fca
lithdew <kenta@lithdew.net>
2021-03-23 03:10:18 +0900

    ed25519_donna: add wrapper methods around c api with tests

--- ed25519_donna/ed25519.zig
+++ ed25519_donna/ed25519.zig
@@ -2,20 +2,7 @@ const std = @import("std");
 const c = @cImport(@cInclude("ed25519.h"));
 
 const crypto = std.crypto;
-
-pub fn addTo(step: *std.build.LibExeObjStep, comptime dir: []const u8) void {
-    step.linkLibC();
-
-    step.addIncludeDir(dir ++ "/lib");
-
-    comptime var defines: []const []const u8 = &[_][]const u8{ "-DED25519_CUSTOMRANDOM", "-DED25519_CUSTOMHASH" };
-
-    if (comptime std.Target.x86.featureSetHas(std.Target.current.cpu.features, .sse2)) {
-        defines = defines ++ [_][]const u8{"-DED25519_SSE2"};
-    }
-
-    step.addCSourceFile(dir ++ "/ed25519.c", defines);
-}
+const testing = std.testing;
 
 export fn @"ed25519_randombytes_unsafe"(ptr: *c_void, len: usize) callconv(.C) void {
     crypto.random.bytes(@ptrCast([*]u8, ptr)[0..len]);
@@ -37,22 +24,45 @@ export fn @"ed25519_hash"(hash: [*]u8, ptr: [*]const u8, len: usize) callconv(.C
     crypto.hash.sha3.Sha3_512.hash(ptr[0..len], @ptrCast(*[64]u8, hash), .{});
 }
 
-test "ed25519_donna" {
-    var sk: [32]u8 = undefined;
-    crypto.random.bytes(&sk);
+pub fn addTo(step: *std.build.LibExeObjStep, comptime dir: []const u8) void {
+    step.linkLibC();
 
-    var pk: [32]u8 = undefined;
-    c.ed25519_publickey(&sk, &pk);
+    step.addIncludeDir(dir ++ "/lib");
 
-    var buf: [1024]u8 = undefined;
-    var signature: [64]u8 = undefined;
+    comptime var defines: []const []const u8 = &[_][]const u8{ "-DED25519_CUSTOMRANDOM", "-DED25519_CUSTOMHASH" };
 
-    var t = try std.time.Timer.start();
-
-    var i: usize = 0;
-    while (i < 1_000) : (i += 1) {
-        c.ed25519_sign(&buf, buf.len, &sk, &pk, &signature);
+    if (comptime std.Target.x86.featureSetHas(std.Target.current.cpu.features, .sse2)) {
+        defines = defines ++ [_][]const u8{"-DED25519_SSE2"};
     }
 
-    std.debug.print("Done! Took {d:.2} ns.\n", .{t.read()});
+    step.addCSourceFile(dir ++ "/ed25519.c", defines);
+}
+
+pub fn derivePublicKey(secret_key: [32]u8) [32]u8 {
+    var public_key: [32]u8 = undefined;
+    c.ed25519_publickey(&secret_key, &public_key);
+    return public_key;
+}
+
+pub fn sign(msg: []const u8, secret_key: [32]u8, public_key: [32]u8) [64]u8 {
+    var signature: [64]u8 = undefined;
+    c.ed25519_sign(msg.ptr, msg.len, &secret_key, &public_key, &signature);
+    return signature;
+}
+
+pub fn open(msg: []const u8, public_key: [32]u8, signature: [64]u8) bool {
+    return c.ed25519_sign_open(msg.ptr, msg.len, &public_key, &signature) == 0;
+}
+
+test "ed25519_donna" {
+    var secret_key: [32]u8 = undefined;
+    crypto.random.bytes(&secret_key);
+
+    var public_key: [32]u8 = derivePublicKey(secret_key);
+    var buf: [1024]u8 = undefined;
+
+    var i: usize = 0;
+    while (i < 100) : (i += 1) {
+        testing.expect(open(&buf, public_key, sign(&buf, secret_key, public_key)));
+    }
 }
