2545c6e878671923d90d53cdb84454e70d6817a3
lithdew <kenta@lithdew.net>
2021-03-23 19:25:08 +0900

    oneshot: add stress test and simplify memory ordering for put()

--- oneshot.zig
+++ oneshot.zig
@@ -31,7 +31,10 @@ pub const Channel = struct {
 
             while (true) {
                 const new_state = switch (state) {
-                    EMPTY => @ptrToInt(&node),
+                    EMPTY => update: {
+                        node.next = null;
+                        break :update @ptrToInt(&node);
+                    },
                     NOTIFIED => {
                         hyperia.pool.schedule(.{}, &node.runnable);
                         break;
@@ -48,13 +51,17 @@ pub const Channel = struct {
     }
 
     pub fn put(self: *Self) void {
-        const state = @atomicRmw(usize, &self.state, .Xchg, NOTIFIED, .AcqRel);
+        const state = @atomicRmw(usize, &self.state, .Xchg, NOTIFIED, .Acquire);
         if (state == EMPTY or state == NOTIFIED) return;
+
+        var batch: zap.Pool.Batch = .{};
 
         var it = @intToPtr(?*Node, state);
         while (it) |node| : (it = node.next) {
-            hyperia.pool.schedule(.{}, &node.runnable);
+            batch.push(&node.runnable);
         }
+
+        hyperia.pool.schedule(.{}, batch);
     }
 };
 
@@ -81,4 +88,118 @@ test "oneshot/channel: multiple waiters" {
     nosuspend await d;
 
     testing.expect(channel.state == Channel.NOTIFIED);
+}
+
+test "oneshot/channel: stress test" {
+    const Waiter = struct {
+        lock: *std.Thread.Mutex,
+        cond: *std.Thread.Condition,
+        count: *usize,
+
+        channel: *Channel,
+        runnable: zap.Pool.Runnable = .{ .runFn = run },
+        frame: @Frame(runAsync) = undefined,
+
+        fn run(runnable: *zap.Pool.Runnable) void {
+            const self = @fieldParentPtr(@This(), "runnable", runnable);
+            self.frame = async self.runAsync();
+        }
+
+        fn runAsync(self: *@This()) void {
+            self.channel.wait();
+
+            suspend {
+                const held = self.lock.acquire();
+                defer held.release();
+
+                self.count.* -= 1;
+                self.cond.signal();
+            }
+        }
+    };
+
+    const Putter = struct {
+        lock: *std.Thread.Mutex,
+        cond: *std.Thread.Condition,
+        count: *usize,
+
+        channel: *Channel,
+        runnable: zap.Pool.Runnable = .{ .runFn = run },
+        frame: @Frame(runAsync) = undefined,
+
+        fn run(runnable: *zap.Pool.Runnable) void {
+            const self = @fieldParentPtr(@This(), "runnable", runnable);
+            self.frame = async self.runAsync();
+        }
+
+        fn runAsync(self: *@This()) void {
+            self.channel.put();
+
+            suspend {
+                const held = self.lock.acquire();
+                defer held.release();
+
+                self.count.* -= 1;
+                self.cond.signal();
+            }
+        }
+    };
+
+    hyperia.init();
+    defer hyperia.deinit();
+
+    const allocator = testing.allocator;
+
+    var test_count: usize = 10_000;
+    var rand = std.rand.DefaultPrng.init(0);
+
+    while (test_count > 0) : (test_count -= 1) {
+        var lock: std.Thread.Mutex = .{};
+        var cond: std.Thread.Condition = .{};
+        var count: usize = 0;
+
+        var waiters = std.ArrayList(Waiter).init(allocator);
+        defer waiters.deinit();
+
+        var putters = std.ArrayList(Putter).init(allocator);
+        defer putters.deinit();
+
+        const waiter_count = rand.random.intRangeAtMost(usize, 4, 10);
+        const putter_count = rand.random.intRangeAtMost(usize, 4, 10);
+
+        count = waiter_count + putter_count;
+
+        var batch: zap.Pool.Batch = .{};
+        var channel: Channel = .{};
+
+        var i: usize = 0;
+        while (i < waiter_count) : (i += 1) {
+            try waiters.append(Waiter{
+                .lock = &lock,
+                .cond = &cond,
+                .count = &count,
+                .channel = &channel,
+            });
+        }
+
+        var j: usize = 0;
+        while (j < putter_count) : (j += 1) {
+            try putters.append(Putter{
+                .lock = &lock,
+                .cond = &cond,
+                .count = &count,
+                .channel = &channel,
+            });
+        }
+
+        for (waiters.items) |*waiter| batch.push(&waiter.runnable);
+        for (putters.items) |*putter| batch.push(&putter.runnable);
+
+        hyperia.pool.schedule(.{}, batch);
+
+        const held = lock.acquire();
+        defer held.release();
+
+        while (count != 0) cond.wait(&lock);
+    }
 }
