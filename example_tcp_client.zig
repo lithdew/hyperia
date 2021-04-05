015d03366f4ae74389e77a0fe1cd3b0f21c7d3f7
lithdew <kenta@lithdew.net>
2021-04-05 10:47:28 +0900

    example_tcp_client: wait for conn if no conn is available, add reportConnected and reportConnectError

--- example_tcp_client.zig
+++ example_tcp_client.zig
@@ -10,6 +10,7 @@ const AsyncWaitGroupAllocator = hyperia.AsyncWaitGroupAllocator;
 const os = std.os;
 const mem = std.mem;
 const net = std.net;
+const meta = std.meta;
 const log = std.log.scoped(.client);
 
 var stopped: bool = false;
@@ -19,7 +20,28 @@ var reactor_event: Reactor.AutoResetEvent = undefined;
 pub const Client = struct {
     pub const capacity = 1;
 
+    pub const Status = enum {
+        open,
+        closed,
+        errored,
+    };
+
+    pub const Waiter = struct {
+        runnable: zap.Pool.Runnable = .{ .runFn = run },
+        frame: anyframe,
+
+        result: Connection.Error!*Connection,
+        next: ?*Waiter = undefined,
+
+        pub fn run(runnable: *zap.Pool.Runnable) void {
+            const self = @fieldParentPtr(Waiter, "runnable", runnable);
+            resume self.frame;
+        }
+    };
+
     pub const Connection = struct {
+        pub const Error = os.ConnectError;
+
         client: *Client,
         socket: AsyncSocket,
         connected: bool = false,
@@ -30,17 +52,29 @@ pub const Client = struct {
         }
 
         pub fn run(self: *Connection) !void {
+            defer {
+                suspend self.deinit();
+            }
+
             self.connect() catch |err| {
-                log.warn("got an error: {}", .{err});
+                self.client.reportConnectError(self, @errorToInt(err));
                 return err;
             };
 
+            if (!self.client.reportConnected(self)) {
+                return;
+            }
+
             log.info("successfully connected", .{});
+
+            self.socket.deinit();
         }
 
         fn connect(self: *Connection) !void {
             self.socket = try AsyncSocket.init(os.AF_INET, os.SOCK_STREAM | os.SOCK_CLOEXEC, os.IPPROTO_TCP);
             errdefer self.socket.deinit();
+
+            try self.socket.setNoDelay(true);
 
             try self.client.reactor.add(self.socket.socket.fd, &self.socket.handle, Reactor.Interest{
                 .readable = true,
@@ -60,6 +94,8 @@ pub const Client = struct {
     len: usize = 0,
 
     wga: AsyncWaitGroupAllocator,
+    waiters: ?*Waiter = null,
+    status: Status = .open,
 
     pub fn init(allocator: *mem.Allocator, reactor: Reactor, address: net.Address) !Client {
         const pool = try allocator.create([capacity]*Connection);
@@ -78,10 +114,13 @@ pub const Client = struct {
         allocator.destroy(@ptrCast(*const [capacity]*Connection, self.pool));
     }
 
-    pub fn acquire(self: *Client) !*Connection {
-        const held = self.lock.acquire();
-        defer held.release();
+    pub const FetchResult = union(enum) {
+        available: *Connection,
+        spawned: *Connection,
+        pending: void,
+    };
 
+    fn tryFetch(self: *Client) !FetchResult {
         if (self.len == 0) {
             const conn = try self.wga.allocator.create(Connection);
             errdefer self.wga.allocator.destroy(conn);
@@ -90,9 +129,128 @@ pub const Client = struct {
 
             self.pool[self.len] = conn;
             self.len += 1;
+
+            self.status = .open;
+
+            return FetchResult{ .spawned = conn };
         }
 
-        return self.pool[0];
+        if (!self.pool[0].connected) {
+            return FetchResult.pending;
+        }
+
+        return FetchResult{ .available = self.pool[0] };
+    }
+
+    pub fn fetch(self: *Client) !*Connection {
+        const held = self.lock.acquire();
+
+        if (self.status == .closed) {
+            held.release();
+            return error.Closed;
+        }
+
+        const result = self.tryFetch() catch |err| {
+            held.release();
+            return err;
+        };
+
+        switch (result) {
+            .available => |conn| {
+                held.release();
+                return conn;
+            },
+            else => {},
+        }
+
+        var waiter: Waiter = .{ .frame = @frame(), .result = undefined };
+        suspend {
+            waiter.next = self.waiters;
+            self.waiters = &waiter;
+            held.release();
+
+            if (result == .spawned) {
+                result.spawned.frame = async result.spawned.run();
+            }
+        }
+
+        return waiter.result;
+    }
+
+    /// Lock must be held, and connection must exist in the pool.
+    fn deregister(self: *Client, conn: *Connection) void {
+        const pool = self.pool[0..self.len];
+        const i = mem.indexOfScalar(*Connection, pool, conn) orelse unreachable;
+
+        log.info("connection {*} was released", .{conn});
+
+        if (i == self.len - 1) {
+            pool[i] = undefined;
+            self.len -= 1;
+            return;
+        }
+
+        pool[i] = pool[self.len - 1];
+        self.len -= 1;
+    }
+
+    fn reportConnectError(self: *Client, conn: *Connection, err: meta.Int(.unsigned, @sizeOf(Connection.Error) * 8)) void {
+        const batch = collect: {
+            var batch: zap.Pool.Batch = .{};
+
+            const held = self.lock.acquire();
+            defer held.release();
+
+            if (self.status != .closed) {
+                self.status = .errored;
+            }
+
+            self.status = .errored;
+            self.deregister(conn);
+
+            // Only the last pooled connection reports an error.
+
+            if (self.len > 0) {
+                return;
+            }
+
+            while (self.waiters) |waiter| : (self.waiters = waiter.next) {
+                waiter.result = @errSetCast(Connection.Error, @intToError(err));
+                batch.push(&waiter.runnable);
+            }
+
+            break :collect batch;
+        };
+
+        hyperia.pool.schedule(.{}, batch);
+    }
+
+    fn reportConnected(self: *Client, conn: *Connection) bool {
+        const batch = collected: {
+            var batch: zap.Pool.Batch = .{};
+
+            const held = self.lock.acquire();
+            defer held.release();
+
+            if (self.status == .closed) {
+                conn.socket.deinit();
+                self.deregister(conn);
+                return false;
+            }
+
+            self.status = .open;
+            conn.connected = true;
+
+            while (self.waiters) |waiter| : (self.waiters = waiter.next) {
+                waiter.result = conn;
+                batch.push(&waiter.runnable);
+            }
+
+            break :collected batch;
+        };
+
+        hyperia.pool.schedule(.{}, batch);
+        return true;
     }
 };
 
@@ -105,10 +263,12 @@ fn runApp(reactor: Reactor) !void {
     var client = try Client.init(hyperia.allocator, reactor, net.Address.initIp4(.{ 0, 0, 0, 0 }, 9000));
     defer client.deinit(hyperia.allocator);
 
-    var conn = try client.acquire();
-    defer conn.deinit();
+    var i: usize = 0;
+    while (i < 100) : (i += 1) {
+        var conn = try client.fetch();
+    }
 
-    conn.frame = async conn.run();
+    log.info("done", .{});
 
     hyperia.ctrl_c.wait();
 }
@@ -145,4 +305,6 @@ pub fn main() !void {
     }
 
     try nosuspend await frame;
+
+    log.info("good bye!", .{});
 }
